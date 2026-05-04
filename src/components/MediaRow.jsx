aa92813956ad118bed558e65ce1c91b7f9ab8384
Trollipopper <113014080+Trollipopper@users.noreply.github.com>
2026-05-04 11:22:10 +0300

    Add Home and MediaRow componens

---
+++ src/components/MediaRow.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+
+const MediaRow = ({ item }) => {
+  return (
+    <tr>
+      <td>
+        <img src={item.thumbnail} alt={item.title} />
+      </td>
+      <td>{item.title}</td>
+      <td>{item.description || '-'}</td>
+      <td>{new Date(item.created_at).toLocaleString('fi-FI')}</td>
+      <td>{item.filesize.toLocaleString()}</td>
+      <td>{item.media_type}</td>
+    </tr>
+  );
+};
+
+MediaRow.propTypes = {
+  item: PropTypes.shape({
+    media_id: PropTypes.number.isRequired,
+    user_id: PropTypes.number,
+    filename: PropTypes.string,
+    thumbnail: PropTypes.string,
+    filesize: PropTypes.number,
+    media_type: PropTypes.string,
+    title: PropTypes.string,
+    description: PropTypes.string,
+    created_at: PropTypes.string,
+  }).isRequired,
+};
+
+export default MediaRow;
